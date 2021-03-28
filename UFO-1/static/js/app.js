6c754cdc7413db854d283fc29294c00a7953a21c
Yash5 <yashalphabetagamma@gmail.com>
2021-03-27 20:08:18 -0700

    app setup

--- UFO-1/static/js/app.js
+++ UFO-1/static/js/app.js
@@ -1,4 +1,11 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+//HTML
+
+var table = d3.select('table');
+var tBody = table.select('tbody');
+var filterBtn = d3.select('#filter-btn');
+
+createTable(tableData, tableBody);
+
